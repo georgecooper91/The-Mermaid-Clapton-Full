e38cab46bc6bbea8d34aeaa3eb19152950fa834d
George Cooper <georgecooper91@gmail.com>
2019-03-02 13:04:54 +0000

    new nav-bar burger

--- public/scripts/mermaidScripts.js
+++ public/scripts/mermaidScripts.js
@@ -1,26 +1,8 @@
-var burger = document.getElementById("burg");
-var x = document.getElementById("x");
-var navMenu = document.querySelector(".navMenu");
-var scroll = document.getElementsByClassName("scroll");
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".navMenu");
 
-burger.addEventListener("click", function(){
-    console.log("Clicked");
-    burger.classList.add("hide");
-    x.classList.remove("hide");
-    navMenu.classList.remove("navMenu");
-    // if(burger.classList === fa-bars){
-    //     burger.classList.add("hide");
-    //     x.classList.remove("hide");
-    //     navMenu.classList.remove("navMenu");
-    // } else {
-    //     burger.classList.remove("hide");
-    //     x.classList.add("hide");
-    //     navMenu.classList.add("navMenu");
-    // }
-});
-
-x.addEventListener("click", function(){
-    burger.classList.remove("hide");
-    x.classList.add("hide");
-    navMenu.classList.add("navMenu");
-});
+//Show and hide nav bar links
+burger.addEventListener('click', () => {
+    navMenu.classList.toggle('navActive');
+    burger.classList.toggle('toggle');
+})
